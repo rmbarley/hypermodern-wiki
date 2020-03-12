f27f9c4ff4a5e5cdd1de98a511cc83d413161cbe
Ryan Barley <ryan.barley@8451.com>
2020-03-12 15:30:32 -0400

    Fail gracefully when request errors

--- src/hypermodern_wiki/console.py
+++ src/hypermodern_wiki/console.py
@@ -1,11 +1,7 @@
+import click
 import textwrap
 
-import click
-import requests
-
-from . import __version__
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+from . import __version__, wikipedia
 
 
 @click.command()
@@ -13,9 +9,7 @@ API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
 def main():
     """Hypermodern python project
     """
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    data = wikipedia.random_page()
 
     title = data["title"]
     extract = data["extract"]
